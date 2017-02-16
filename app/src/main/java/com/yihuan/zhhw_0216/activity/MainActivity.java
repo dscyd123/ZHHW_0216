d1808393cd7786a2245a4925441971b7cce72692
dscyd <245462439@qq.com>
2017-02-16 16:59:30 +0800

    Initial commit

---
+++ app/src/main/java/com/yihuan/zhhw_0216/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yihuan.zhhw_0216.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yihuan.zhhw_0216.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
